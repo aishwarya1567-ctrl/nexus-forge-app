22fe00b0d32ae554d7623a9b869178051ccff2df
aishwarya1567-ctrl <aishwaryasubramanian1567@gmail.com>
2026-06-23 16:24:11 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,36 @@
 function login() {
-  alert("Login Successful!");
+  const username = document.getElementById("username").value;
+  const password = document.getElementById("password").value;
+
+  if (username && password) {
+    alert("Login Successful");
+  } else {
+    alert("Please enter credentials");
+  }
+}
+
+let staff = [];
+
+function addStaff() {
+  const name = document.getElementById("staffName").value;
+  if (!name) return;
+
+  staff.push(name);
+  renderStaff();
+}
+
+function renderStaff() {
+  const list = document.getElementById("staffList");
+  list.innerHTML = "";
+
+  staff.forEach((s, index) => {
+    const li = document.createElement("li");
+    li.innerHTML = `${s} <button onclick="deleteStaff(${index})">Delete</button>`;
+    list.appendChild(li);
+  });
+}
+
+function deleteStaff(index) {
+  staff.splice(index, 1);
+  renderStaff();
 }
