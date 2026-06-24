ba601fb7acac9ef15041e85241d5c0cb0bebb639
aishwarya1567-ctrl <aishwaryasubramanian1567@gmail.com>
2026-06-24 13:58:32 +0530

    Refactor staff management functions and UI

--- script.js
+++ script.js
@@ -1,22 +1,22 @@
-function login() {
-  const username = document.getElementById("username").value;
-  const password = document.getElementById("password").value;
-
-  if (username && password) {
-    alert("Login Successful");
-  } else {
-    alert("Please enter credentials");
-  }
-}
-
 let staff = [];
 
 function addStaff() {
-  const name = document.getElementById("staffName").value;
-  if (!name) return;
-
+  const name = document.getElementById("name").value;
   staff.push(name);
   renderStaff();
+}
+
+function deleteStaff(index) {
+  staff.splice(index, 1);
+  renderStaff();
+}
+
+function editStaff(index) {
+  const newName = prompt("Enter new name:");
+  if (newName) {
+    staff[index] = newName;
+    renderStaff();
+  }
 }
 
 function renderStaff() {
@@ -25,12 +25,9 @@ function renderStaff() {
 
   staff.forEach((s, index) => {
     const li = document.createElement("li");
-    li.innerHTML = `${s} <button onclick="deleteStaff(${index})">Delete</button>`;
+    li.innerHTML = `${s} 
+      <button onclick="deleteStaff(${index})">Delete</button>
+      <button onclick="editStaff(${index})">Edit</button>`;
     list.appendChild(li);
   });
-}
-
-function deleteStaff(index) {
-  staff.splice(index, 1);
-  renderStaff();
 }
